8e4943bf19970a7f5dc7c8267b6ed79ecef41499
shahinsamiur <shahinsamiur@yahoo.com>
2026-05-02 14:47:49 +0600

    fix(inspirations and navbar): small ui fixed in inspirations page and increase navigation button padding for large screen size

--- app/componnent/Carousel.jsx
+++ app/componnent/Carousel.jsx
@@ -7,73 +7,96 @@ import hero4 from "../../public/ronaldo.webp";
 import hero1 from "../../public/tranding.png";
 import hero5 from "../../public/tredingTwo.png";
 
-
-
 export default function Carousel() {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 2000,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        cssEase: "ease-in-out",
-        arrows: false,
-        draggable: true,
-        swipeToSlide: true,
-        pauseOnHover: true,
-        responsive: [
-            { breakpoint: 1024, settings: { slidesToShow: 2 } },
-            { breakpoint: 640, settings: { slidesToShow: 1 } },
-        ],
-    };
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    cssEase: "ease-in-out",
+    arrows: false,
+    draggable: true,
+    swipeToSlide: true,
+    pauseOnHover: true,
+    responsive: [
+      { breakpoint: 1024, settings: { slidesToShow: 2 } },
+      { breakpoint: 640, settings: { slidesToShow: 1 } },
+    ],
+  };
 
+  const items = [
+    {
+      id: 1,
+      img: hero5,
+      title: "Family Game Night Deck",
+      desc: "“Custom cards for every family member—turned our gatherings into a tradition!”",
+    },
+    {
+      id: 2,
+      img: hero4,
+      title: "Family Game Night Deck",
+      desc: "“Custom cards for every family member—turned our gatherings into a tradition!”",
+    },
+    {
+      id: 3,
+      img: hero3,
+      title: "Family Game Night Deck",
+      desc: "“Custom cards for every family member—turned our gatherings into a tradition!”",
+    },
+    {
+      id: 4,
+      img: hero2,
+      title: "Family Game Night Deck",
+      desc: "“Custom cards for every family member—turned our gatherings into a tradition!”",
+    },
+    {
+      id: 5,
+      img: hero1,
+      title: "Family Game Night Deck",
+      desc: "“Custom cards for every family member—turned our gatherings into a tradition!”",
+    },
+  ];
 
-    const items = [
-        { id: 1, img: hero5, title: "Family Game Night Deck", desc: "“Custom cards for every family member—turned our gatherings into a tradition!”" },
-        { id: 2, img: hero4, title: "Family Game Night Deck", desc: "“Custom cards for every family member—turned our gatherings into a tradition!”" },
-        { id: 3, img: hero3, title: "Family Game Night Deck", desc: "“Custom cards for every family member—turned our gatherings into a tradition!”" },
-        { id: 4, img: hero2, title: "Family Game Night Deck", desc: "“Custom cards for every family member—turned our gatherings into a tradition!”" },
-        { id: 5, img: hero1, title: "Family Game Night Deck", desc: "“Custom cards for every family member—turned our gatherings into a tradition!”" },
+  return (
+    <section className="bg-gradient-to-b from-[#EBF6FE] to-[#f2f9ff29]">
+      <div className="pt-[120px] pb-[100px]  max-w-7xl mx-auto">
+        <div className="text-center text-[#333333] mb-12">
+          <h1 className="text-5xl uppercase font-bold">
+            Bringing Ideas to <span className="text-[#3CA9FF]">Life</span>
+          </h1>
+          <p className="text-gray-500 text-lg mt-4">
+            Real stories, stunning designs, and unforgettable moments—see how
+            the Momento community creates, plays, and connects.
+          </p>
+        </div>
 
-    ];
-
-    return (
-        <section className="bg-gradient-to-b from-[#EBF6FE] to-[#f2f9ff29]">
-            <div className="pt-[120px] pb-[100px]  max-w-7xl mx-auto">
-                <div className="text-center text-[#333333] mb-12">
-                    <h1 className="text-5xl uppercase font-bold">
-                        Bringing Ideas to <span className="text-[#3CA9FF]">Life</span>
-                    </h1>
-                    <p className="text-gray-500 text-lg mt-4">
-                        Real stories, stunning designs, and unforgettable moments—see how the Momento
-                        community creates, plays, and connects.
-                    </p>
+        <div className="px-6 w-full ">
+          <Slider {...settings}>
+            {items.map((item) => (
+              <div key={item.id} className="px-3 pb-12  ">
+                <div className="bg-white rounded-xl shadow-md overflow-hidden ">
+                  <Image
+                    src={item.img}
+                    alt={item.title}
+                    width={500}
+                    height={400}
+                    className="w-full h-80 object-contain bg-sky-100 p-5"
+                  />
+                  <div className="p-5 text-center">
+                    <h2 className="text-xl font-bold text-[#333]">
+                      {item.title}
+                    </h2>
+                    <p className="text-gray-600 mt-2 text-sm">{item.desc}</p>
+                  </div>
                 </div>
-
-                <div className="px-6">
-                    <Slider {...settings}>
-                        {items.map((item) => (
-                            <div key={item.id} className="px-3 pb-12">
-                                <div className="bg-white rounded-xl shadow-md overflow-hidden">
-                                    <Image
-                                        src={item.img}
-                                        alt={item.title}
-                                        width={500}
-                                        height={400}
-                                        className="w-full h-80 object-contain bg-sky-100 p-5"
-                                    />
-                                    <div className="p-5 text-center">
-                                        <h2 className="text-xl font-bold text-[#333]">{item.title}</h2>
-                                        <p className="text-gray-600 mt-2 text-sm">{item.desc}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </Slider>
-                </div>
-            </div>
-        </section>
-    );
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </section>
+  );
 }
--- app/componnent/Navigation.jsx
+++ app/componnent/Navigation.jsx
@@ -1,75 +1,78 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const Navigation = ({ isOpen, setisOpen }) => {
+  const navItems = [
+    {
+      name: "Home",
+      nested: false,
+      sub: [],
+      link: "/",
+    },
+    {
+      name: "About",
+      nested: false,
+      sub: [],
+      link: "/about",
+    },
+    {
+      name: "Shop",
+      nested: false,
+      sub: [],
+      link: "/shop",
+    },
+    {
+      name: "How it Works",
+      nested: false,
+      sub: [],
+      link: "/howitwork",
+    },
+    // {
+    //     name: "Card Customization",
+    //     nested: false,
+    //     sub: [],
+    //     link: "/card-customization"
+    // },
+    {
+      name: "Inspirations",
+      nested: false,
+      sub: [],
+      link: "/inspirations",
+    },
+    {
+      name: "Contact Us",
+      nested: false,
+      sub: [],
+      link: "/contact",
+    },
+  ];
 
+  const pathname = usePathname();
 
-    const navItems = [
-        {
-            name: "Home",
-            nested: false,
-            sub: [],
-            link: "/"
-        },
-        {
-            name: "About",
-            nested: false,
-            sub: [],
-            link: "/about"
-        },
-        {
-            name: "Shop",
-            nested: false,
-            sub: [],
-            link: "/shop"
-        },
-        {
-            name: "How it Works",
-            nested: false,
-            sub: [],
-            link: "/howitwork"
-        },
-        // {
-        //     name: "Card Customization",
-        //     nested: false,
-        //     sub: [],
-        //     link: "/card-customization"
-        // },
-        {
-            name: "Inspirations",
-            nested: false,
-            sub: [],
-            link: "/inspirations"
-        },
-        {
-            name: "Contact Us",
-            nested: false,
-            sub: [],
-            link: "/contact"
-        },
-    ]
-
-
-    const pathname = usePathname();
-
-
-    return (
-        <nav className={`${isOpen ? "flex items-start" : "hidden"} lg:flex lg:items-center h-screen w-screen lg:w-fit lg:h-full absolute top-[75px] left-0 lg:static h-screen bg-white border border-r border-gray-300 lg:border-0`}>
-            <div className="flex flex-col lg:flex-row items-start lg:items-center gap-4 lg:gap-7 text-gray-500 mt-10 lg:mt-0 px-4 pr-6 lg:pr-0 lg:pl-0 w-full">
-
-                {
-                    navItems?.map((item, index) => {
-                        return (
-                            <Link key={index} onClick={() => { setisOpen(false) }} className={`font-semibold text-md text-nowrap py-3 px-2 rounded-md lg:px-1 lg:py-0 hover:bg-sky-100 w-full ${pathname == item?.link && 'bg-sky-100'}`} href={item?.link}>{item?.name}</Link>
-                        )
-                    })
-                }
-
-            </div>
-        </nav >
-    )
-}
+  return (
+    <nav
+      className={`${isOpen ? "flex items-start" : "hidden"} lg:flex lg:items-center h-screen w-screen lg:w-fit lg:h-full absolute top-[75px] left-0 lg:static h-screen bg-white border border-r border-gray-300 lg:border-0`}
+    >
+      <div className="flex flex-col lg:flex-row items-start lg:items-center gap-4 lg:gap-7 text-gray-500 mt-10 lg:mt-0 px-4 pr-6 lg:pr-0 lg:pl-0 w-full">
+        {navItems?.map((item, index) => {
+          return (
+            <Link
+              key={index}
+              onClick={() => {
+                setisOpen(false);
+              }}
+              className={`font-semibold text-md text-nowrap py-3 px-2 rounded-md lg:px-2 lg:py-1 hover:bg-sky-100 w-full ${pathname == item?.link && "bg-sky-100"}`}
+              href={item?.link}
+            >
+              {item?.name}
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
+  );
+};
 
 export default Navigation;

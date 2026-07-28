274e1a39a7bc4310edf957a3750026e115d43f9c
funkyd3v <funkyd3v@gmail.com>
2026-07-28 14:50:15 +0600

    fix: dashboard typo

--- middleware.js
+++ middleware.js
@@ -5,7 +5,7 @@ async function verifyJWT(token) {
     try {
         const secret = new TextEncoder().encode(process.env.NEXT_PUBLIC_JWT_SECRET);
         const { payload } = await jwtVerify(token, secret, {
-            clockTolerance: 30 // allow 30 seconds difference
+            clockTolerance: 30
         });
         return payload;
     } catch (err) {
